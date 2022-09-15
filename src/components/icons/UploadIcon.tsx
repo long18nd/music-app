f317080eb08ec8d8b9d735dc3d015dd2891b885d
Luu Ngoc Long <longln@vistarsoft.com>
2022-09-15 13:26:48 +0700

    create Header component

---
+++ src/components/icons/UploadIcon.tsx
@@ -0,0 +1,6 @@
+import UploadSvg from "@/assets/icons/upload.svg?component";
+import SvgIcon, { SvgIconProps } from "@/components/SvgIcon";
+
+const UploadIcon = (props: SvgIconProps) => <SvgIcon svg={UploadSvg} {...props} />;
+
+export default UploadIcon;
